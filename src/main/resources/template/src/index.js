bfe8a435193e996d878f412b3bb83a6a65ff3ca6
geonhui.kim <geonhui.kim@navercorp.com>
2020-07-29 10:45:56 +0900

    Export main function

--- src/main/resources/template/src/index.js
+++ src/main/resources/template/src/index.js
@@ -1,6 +1,7 @@
 function main(params) {
-    msg = "Hello, " + params.name + " from " + params.place;
-    family = "You have " + params.children + " children ";
-    stats = "and are " + params.height + " m. tall.";
-    return { greeting:  msg, details: family + stats };
+    const msg = 'Hello, ' + params.name + ' from ' + params.place;
+    const family = 'You have ' + params.children + ' children ';
+    const stats = 'and are ' + params.height + ' m. tall.';
+    return { greeting: msg, details: family + stats };
 }
+module.exports.main = main;
