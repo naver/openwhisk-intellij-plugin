9eb59535f6e829e58d3583d3266afacff06bbd8e
geonhui.kim <geonhui.kim@navercorp.com>
2020-11-03 16:41:19 +0900

    Use svg file for icon

--- src/main/java/com/navercorp/openwhisk/intellij/common/Icons.java
+++ src/main/java/com/navercorp/openwhisk/intellij/common/Icons.java
@@ -40,6 +40,7 @@ public class Icons {
     public static final Icon OPEN_DISK_HOVER = load("/icons/openDiskHover.svg");
     public static final Icon RUN_ANYTHING = load("/icons/run_anything.svg");
     public static final Icon YAML = load("/icons/yaml.svg");
+    public static final Icon MENU_PASTE = load("/icons/menu-paste.svg");
 
     public static final Icon KIND_JAVA = load("/icons/language/java/java.svg");
     public static final Icon KIND_JS = load("/icons/language/javascript/javascript.svg");
