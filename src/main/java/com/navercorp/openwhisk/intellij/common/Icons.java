d0113a30a8411373249edaa49ad78e87ac7ba6d0
geonhui.kim <geonhui.kim@navercorp.com>
2020-12-23 18:00:54 +0900

    Replace deprecated function with new one

--- src/main/java/com/navercorp/openwhisk/intellij/common/Icons.java
+++ src/main/java/com/navercorp/openwhisk/intellij/common/Icons.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2020-present NAVER Corp.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,6 +22,7 @@ import gnu.trove.THashMap;
 
 import javax.swing.*;
 import java.util.Map;
+import java.util.Objects;
 
 public class Icons {
 
@@ -54,7 +55,8 @@ public class Icons {
 
     private static Icon load(String path) {
         try {
-            return IconLoader.getIcon(path);
+            Icon icon = IconLoader.findIcon(path);
+            return Objects.requireNonNullElse(icon, AllIcons.General.Warning);
         } catch (Throwable t) {
             return AllIcons.General.Warning;
         }
